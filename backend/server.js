aa6566fb8a34e0b5a6e7c272483d4fa2e98caa27
Kyle Yin <kyle@KyledeMacBook-Pro.local>
2023-04-30 22:32:44 +0800

    roll back

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,38 @@ const mysql = require("mysql");
 const cors = require("cors");
 
 const app = express();
+
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:5000");
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
+});
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: ["*"],
+    methods: ["POST, GET"],
+    credentials: true,
+  })
+);
 
 const db = mysql.createConnection({
   host: "localhost",
@@ -21,15 +51,19 @@ app.get("/product_list", (req, res) => {
   });
 });
 
-app.post("/user", (req, res) => {
-  const sql = "SELECT * FROM user WHERE user_email = ? AND user_password = ?";
-
+app.post("/login", (req, res) => {
+  const sql = "SELECT * FROM user WHERE email = ? AND password = ?";
   db.query(sql, [req.body.email, req.body.password], (err, data) => {
     if (err) return res.json("Error");
     if (data.length > 0) {
-      return res.json("Login Successfully");
+      const name = data[0].name;
+      const token = Jwt.sign({ name }, "our-jsonwebtoken-secret-key", {
+        expiresIn: "1d",
+      });
+      res.cookie("token", token);
+      return res.json({ Status: "Success" });
     } else {
-      return res.json("Login Failed");
+      return res.json({ Message: "Login Failed" });
     }
   });
 });
--- frontend/src/LoginPage/Login.js
+++ frontend/src/LoginPage/Login.js
@@ -1,21 +1,33 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "../LoginPage/Login.scss";
 import axios from "axios";
 
 export default function Login() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+  });
+  const navigate = useNavigate();
+  axios.defaults.withCredentials = true;
+
   function handleSubmit(event) {
     event.preventDefault();
     axios
-      .post("http://localhost:5000/user", { email, password })
-      .then((res) => console.log(res))
+      .post("http://127.0.0.1:5000/login", values)
+      .then((res) => {
+        if (res.data.Status === "Success") {
+          navigate("/");
+        } else {
+          alert(res.data.Message);
+        }
+      })
       .catch((err) => console.log(err));
   }
 
   return (
     <div className="login-body">
-      <div class="wrapper">
+      <div className="wrapper">
         <div className="from-box login">
           <h2>Login</h2>
           <form onSubmit={handleSubmit}>
@@ -23,7 +35,9 @@ export default function Login() {
               <input
                 type="email"
                 required
-                onChange={(e) => setEmail(e.target.value)}
+                onChange={(e) =>
+                  setValues({ ...values, email: e.target.value })
+                }
               />
               <label>Email</label>
             </div>
@@ -31,7 +45,9 @@ export default function Login() {
               <input
                 type="password"
                 required
-                onChange={(e) => setPassword(e.target.value)}
+                onChange={(e) =>
+                  setValues({ ...values, password: e.target.value })
+                }
               />
               <label>Password</label>
             </div>
