374d3dbadc81c6d2496c5c0a183097b66c2db18e
Kyle Yin <kyle@KyledeMacBook-Pro.local>
2023-05-07 15:46:01 +0800

    login session in header

--- frontend/src/Header.js
+++ frontend/src/Header.js
@@ -1,7 +1,39 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import "./Css/Header.scss";
 
 export default function Header() {
+  const [auth, setAuth] = useState(false);
+  const [name, setName] = useState("");
+  const [message, setMessage] = useState("");
+
+  axios.defaults.withCredentials = true;
+
+  useEffect(() => {
+    axios.get("http://localhost:3001").then((res) => {
+      if (res.data.Status === "Success") {
+        setAuth(true);
+        setName(res.data.name);
+      } else {
+        setAuth(false);
+        setMessage(res.data.Message);
+      }
+    });
+  }, []);
+
+  const handleLogout = () => {
+    axios
+      .get("http://localhost:3001/logout")
+      .then((res) => {
+        if (res.data.Status === "Success") {
+          window.location.reload();
+        } else {
+          alert("error");
+        }
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
     <header>
       <h2 className="logo">Logo</h2>
@@ -10,7 +42,17 @@ export default function Header() {
         <a href="/product">Product</a>
         <a href="/about">About</a>
         <a href="/contact">Contact</a>
-        <a href="/login">Login</a>
+
+        {auth ? (
+          <>
+            <a>{name}</a>
+            <a href="/" onClick={handleLogout}>
+              Logout
+            </a>
+          </>
+        ) : (
+          <a href="/login">Login</a>
+        )}
       </nav>
     </header>
   );
